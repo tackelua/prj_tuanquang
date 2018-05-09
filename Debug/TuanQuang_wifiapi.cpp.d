7f4ee28a290501c666601a1c043c36494cffb637
tackelua <tackelua@gmail.com>
2018-05-09 14:44:03 +0700

    add ajax

--- Debug/TuanQuang_wifiapi.cpp.d
+++ Debug/TuanQuang_wifiapi.cpp.d
@@ -59,62 +59,6 @@ C:\Users\tacke\AppData\Local\Temp\VMBuilds\TuanQuang_wifiapi\esp8266_d1_mini\Deb
  C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/debug.h \
  C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\variants\d1_mini/pins_arduino.h \
  C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\variants\d1_mini/../generic/common.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266mDNS/ESP8266mDNS.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFi.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/include/wl_definitions.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/IPAddress.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Printable.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFiType.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/queue.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFiSTA.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFiGeneric.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFiAP.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFiScan.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/WiFiClient.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Print.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Client.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/IPAddress.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/include/slist.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/WiFiServer.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Server.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/WiFiServerSecure.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/WiFiClientSecure.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/include/ssl.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/WiFiUdp.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Udp.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/include/slist.h \
- C:\Users\tacke\Documents\Arduino\libraries\WiFiManager/WiFiManager.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WebServer\src/ESP8266WebServer.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WebServer\src/detail/RequestHandler.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\DNSServer\src/DNSServer.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/user_interface.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/os_type.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/ets_sys.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/ip_addr.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/opt.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwipopts.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/gluedebug.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/debug.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/arch.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/arch/cc.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip-err-t.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/mem.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/gluedebug.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/limits.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/arch/cc.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip-git-hash.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/time.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/debug.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/def.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/ip4_addr.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/ip6_addr.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/def.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/queue.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/spi_flash.h \
- C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/gpio.h \
- C:\Users\tacke\Documents\Arduino\libraries\firebase-arduino\src/FirebaseArduino.h \
- C:\Users\tacke\Documents\Arduino\libraries\firebase-arduino\src/Firebase.h \
- C:\Users\tacke\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson.h \
  C:\Users\tacke\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson.hpp \
  C:\Users\tacke\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/DynamicJsonBuffer.hpp \
  C:\Users\tacke\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/JsonBufferBase.hpp \
@@ -193,6 +137,62 @@ C:\Users\tacke\AppData\Local\Temp\VMBuilds\TuanQuang_wifiapi\esp8266_d1_mini\Deb
  C:\Users\tacke\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Polyfills/parseFloat.hpp \
  C:\Users\tacke\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Polyfills/parseInteger.hpp \
  C:\Users\tacke\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson/Serialization/JsonSerializerImpl.hpp \
+ C:\Users\tacke\Documents\Arduino\libraries\ArduinoJson\src/ArduinoJson.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266mDNS/ESP8266mDNS.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFi.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/include/wl_definitions.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/IPAddress.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Printable.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFiType.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/queue.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFiSTA.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFiGeneric.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFiAP.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/ESP8266WiFiScan.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/WiFiClient.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Print.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Client.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/IPAddress.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/include/slist.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/WiFiServer.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Server.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/WiFiServerSecure.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/WiFiClientSecure.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/include/ssl.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/WiFiUdp.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\cores\esp8266/Udp.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WiFi\src/include/slist.h \
+ C:\Users\tacke\Documents\Arduino\libraries\WiFiManager/WiFiManager.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WebServer\src/ESP8266WebServer.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\ESP8266WebServer\src/detail/RequestHandler.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1\libraries\DNSServer\src/DNSServer.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/user_interface.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/os_type.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/ets_sys.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/ip_addr.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/opt.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwipopts.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/gluedebug.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/debug.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/arch.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/arch/cc.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip-err-t.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/mem.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/gluedebug.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/limits.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/arch/cc.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip-git-hash.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/time.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/debug.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/def.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/ip4_addr.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/ip6_addr.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/lwip2/include/lwip/def.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/queue.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/spi_flash.h \
+ C:\Users\tacke\AppData\Local\arduino15\packages\esp8266\hardware\esp8266\2.4.1/tools/sdk/include/gpio.h \
+ C:\Users\tacke\Documents\Arduino\libraries\firebase-arduino\src/FirebaseArduino.h \
+ C:\Users\tacke\Documents\Arduino\libraries\firebase-arduino\src/Firebase.h \
  C:\Users\tacke\Documents\Arduino\libraries\firebase-arduino\src/FirebaseHttpClient.h \
  C:\Users\tacke\Documents\Arduino\libraries\firebase-arduino\src/FirebaseError.h \
  C:\Users\tacke\Documents\Arduino\libraries\firebase-arduino\src/FirebaseObject.h
--- TuanQuang_wifiapi.ino
+++ TuanQuang_wifiapi.ino
@@ -17,6 +17,8 @@
 // FirebaseDemo_ESP8266 is a sample that demo the different functions
 // of the FirebaseArduino API.
 
+#include <ArduinoJson.hpp>
+#include <ArduinoJson.h>
 #include <ESP8266mDNS.h>
 #include <WiFiManager.h>
 #include <ESP8266WebServer.h>
@@ -26,8 +28,8 @@
 // Set these to run example.
 #define FIREBASE_HOST "tuanquang-wifiapi.firebaseio.com"
 #define FIREBASE_AUTH "33Hx0B3ywsAgHVoBT412VStfI9esAyn6ZA0cqBlB"
-#define WIFI_SSID "Gith"
-#define WIFI_PASSWORD "12345678()"
+#define WIFI_SSID "IoT Wifi"
+#define WIFI_PASSWORD "mic@dtu12345678()"
 
 
 ESP8266WebServer server(80);
@@ -35,6 +37,7 @@ ESP8266WebServer server(80);
 const int led = LED_BUILTIN;
 
 void handleRoot() {
+	Serial.println("/");
 	digitalWrite(led, 1);
 	FirebaseObject data = Firebase.get("/"); //đọc database
 	if (Firebase.success()) { //nếu đọc thành công thì tách dữ liệu ra thành từng biến
@@ -58,7 +61,7 @@ void handleRoot() {
 		String note5 = data.getString("note5");
 
 		//html là chuỗi của trang web hiển thị
-		String html = F("<!DOCTYPE html><html><head><title>Title of the document</title><meta charset=\"utf-8\"/><style>#wrap{padding:40px;height:300px}.title{border:1px solid #000;display:inline-block;padding:10px 10px 40px 10px;color:#000;font-weight:bold}#content{margin-top:20px}#left{width:48%;float:left}#right{width:48%;float:right}#stoprobot{width:48%;float:left}#runrobot{width:48%;float:right}#right button{}</style></head><body><div id=\"wrap\"><div style=\"text-align:center;\"><div class=\"title\">Giám sát hoạt động của robot delta</div><div>Account đang đăng nhập: {acc}</div></div><div id=\"content\"><div id=\"left\"><table width=\"100%\" border=\"1\"><thead><tr><th>STT phôi</th><th>Mã phôi</th><th>Tình trạng</th><th>Ghi chú</th></tr></thead><tbody><tr><td>1</td><td>{phoi1}</td><td>{stt1}</td><td>{note1}</td></tr><tr><td>2</td><td>{phoi2}</td><td>{stt2}</td><td>{note2}</td></tr><tr><td>3</td><td>{phoi3}</td><td>{stt3}</td><td>{note3}</td></tr><tr><td>4</td><td>{phoi4}</td><td>{stt4}</td><td>{note4}</td></tr><tr><td>5</td><td>{phoi5}</td><td>{stt5}</td><td>{note5}</td></tr></tbody></table></div><div id=\"right\"><p>Phôi hiện đang xử lý: {num_xuly}</p><p>Số phôi đã xử lý: {num_done}</p><div id=\"stoprobot\"> <canvas id=\"stopcanvas\" width=\"200\" height=\"200\"></canvas> <button>Ngưng robot</button></div><div id=\"runrobot\"> <canvas id=\"runcanvas\" width=\"200\" height=\"200\"></canvas> <button>Chạy robot</button></div></div></div></div></body> <script type=\"text/javascript\">var canvas=document.getElementById('stopcanvas');var context=canvas.getContext('2d');var centerX=canvas.width/2;var centerY=canvas.height/2;var radius=70;context.beginPath();context.arc(centerX,centerY,radius,0,2*Math.PI,false);context.fillStyle='red';context.fill();context.lineWidth=5;context.strokeStyle='red';context.stroke();var canvas1=document.getElementById('runcanvas');var context1=canvas1.getContext('2d');var centerX1=canvas1.width/2;var centerY1=canvas1.height/2;var radius1=70;context1.beginPath();context1.arc(centerX,centerY,radius,0,2*Math.PI,false);context1.fillStyle='#5a9bd5';context1.fill();context1.lineWidth=5;context1.strokeStyle='#5a9bd5';context1.stroke();</script> </body></html> </script></html>");
+		String html = F("<!DOCTYPE html><html><head> <title>Title of the document</title> <meta charset=\"utf-8\"/> <style>#wrap{padding: 40px; height: 300px;}.title{border: 1px solid #000; display: inline-block; padding: 10px 10px 40px 10px; color: #000; font-weight: bold;}#content{margin-top: 20px;}#left{width: 48%; float: left;}#right{width: 48%; float: right;}#stoprobot{width: 48%; float: left;}#runrobot{width: 48%; float: right;}#right button{}</style></head><body> <div id=\"wrap\"> <div style=\"text-align:center;\"> <div class=\"title\">Giám sát hoạt động của robot delta</div><div>Account đang đăng nhập: <spand id=\"acc\">{acc}</span> </div></div><div id=\"content\"> <div id=\"left\"> <table width=\"100%\" border=\"1\"> <thead> <tr> <th>STT phôi</th> <th>Mã phôi</th> <th>Tình trạng</th> <th>Ghi chú</th> </tr></thead> <tbody> <tr> <td>1</td><td id=\"phoi1\">{phoi1}</td><td id=\"stt1\">{stt1}</td><td id=\"note1\">{note1}</td></tr><tr> <td>2</td><td id=\"phoi2\">{phoi2}</td><td id=\"stt2\">{stt2}</td><td id=\"note2\">{note2}</td></tr><tr> <td>3</td><td id=\"phoi3\">{phoi3}</td><td id=\"stt3\">{stt3}</td><td id=\"note3\">{note3}</td></tr><tr> <td>4</td><td id=\"phoi4\">{phoi4}</td><td id=\"stt4\">{stt4}</td><td id=\"note4\">{note4}</td></tr><tr> <td>5</td><td id=\"phoi5\">{phoi5}</td><td id=\"stt5\">{stt5}</td><td id=\"note5\">{note5}</td></tr></tbody> </table> </div><div id=\"right\"> <p>Phôi hiện đang xử lý: <span id=\"num_xuly\">{num_xuly}</span></p><p>Số phôi đã xử lý: <span id=\"num_done\">{num_done}</span></p><div id=\"stoprobot\"> <canvas id=\"stopcanvas\" width=\"200\" height=\"200\"></canvas> <button>Ngưng robot</button> </div><div id=\"runrobot\"> <canvas id=\"runcanvas\" width=\"200\" height=\"200\"></canvas> <button>Chạy robot</button> </div></div></div></div></body><script type=\"text/javascript\">var canvas=document.getElementById('stopcanvas'); var context=canvas.getContext('2d'); var centerX=canvas.width / 2; var centerY=canvas.height / 2; var radius=70; context.beginPath(); context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false); context.fillStyle='red'; context.fill(); context.lineWidth=5; context.strokeStyle='red'; context.stroke(); var canvas1=document.getElementById('runcanvas'); var context1=canvas1.getContext('2d'); var centerX1=canvas1.width / 2; var centerY1=canvas1.height / 2; var radius1=70; context1.beginPath(); context1.arc(centerX, centerY, radius, 0, 2 * Math.PI, false); context1.fillStyle='#5a9bd5'; context1.fill(); context1.lineWidth=5; context1.strokeStyle='#5a9bd5'; context1.stroke(); setInterval(function(){updateData();}, 1000); function updateData(){var xhttp=new XMLHttpRequest(); xhttp.onreadystatechange=function(){if (this.readyState==4 && this.status==200){var obj=JSON.parse(this.responseText); document.getElementById(\"acc\").innerHTML=obj.acc; document.getElementById(\"num_done\").innerHTML=obj.num_done; document.getElementById(\"num_xuly\").innerHTML=obj.num_xuly; document.getElementById(\"phoi1\").innerHTML=obj.phoi1; document.getElementById(\"phoi2\").innerHTML=obj.phoi2; document.getElementById(\"phoi3\").innerHTML=obj.phoi3; document.getElementById(\"phoi4\").innerHTML=obj.phoi4; document.getElementById(\"phoi5\").innerHTML=obj.phoi5; document.getElementById(\"stt1\").innerHTML=obj.stt1; document.getElementById(\"stt2\").innerHTML=obj.stt2; document.getElementById(\"stt3\").innerHTML=obj.stt3; document.getElementById(\"stt4\").innerHTML=obj.stt4; document.getElementById(\"stt5\").innerHTML=obj.stt5; document.getElementById(\"note1\").innerHTML=obj.note1; document.getElementById(\"note2\").innerHTML=obj.note2; document.getElementById(\"note3\").innerHTML=obj.note3; document.getElementById(\"note4\").innerHTML=obj.note4; document.getElementById(\"note5\").innerHTML=obj.note5;}}; xhttp.open(\"GET\", \"/apidata\", true); xhttp.send();}</script></html>");
 		
 		//thay thế data trong code html từ database lấy ở trên
 		html.replace("{acc}", acc);
@@ -85,6 +88,45 @@ void handleRoot() {
 	}
 	else {
 		server.send(200, "text/plain", "Read database fail. Please try again");
+	}
+	digitalWrite(led, 0);
+}
+
+void handleGetDataApi() {
+	Serial.println("/apidata");
+	digitalWrite(led, 1);
+	FirebaseObject data = Firebase.get("/"); //đọc database
+	if (Firebase.success()) { //nếu đọc thành công thì tách dữ liệu ra thành từng biến
+		StaticJsonBuffer<200> jsBuffer;
+		JsonObject& jsData = jsBuffer.createObject();
+
+		jsData["acc"] = data.getString("acc");
+		jsData["num_done"] = data.getString("num_done");
+		jsData["num_xuly"] = data.getString("num_xuly");
+		jsData["phoi1"] = data.getString("phoi1");
+		jsData["phoi2"] = data.getString("phoi2");
+		jsData["phoi3"] = data.getString("phoi3");
+		jsData["phoi4"] = data.getString("phoi4");
+		jsData["phoi5"] = data.getString("phoi5");
+		jsData["stt1"] = data.getString("stt1");
+		jsData["stt2"] = data.getString("stt2");
+		jsData["stt3"] = data.getString("stt3");
+		jsData["stt4"] = data.getString("stt4");
+		jsData["stt5"] = data.getString("stt5");
+		jsData["note1"] = data.getString("note1");
+		jsData["note2"] = data.getString("note2");
+		jsData["note3"] = data.getString("note3");
+		jsData["note4"] = data.getString("note4");
+		jsData["note5"] = data.getString("note5");
+
+		String jsDataStr;
+		jsData.printTo(jsDataStr);
+
+		//gửi xuống client
+		server.send(200, "text/plain", jsDataStr);
+	}
+	else {
+		server.send(404, "text/plain", "{}");
 	}
 	digitalWrite(led, 0);
 }
@@ -127,7 +169,7 @@ void setup() {
 	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 	//WiFiManager wifiManager;
 	//wifiManager.autoConnect("TUANQUANG");
-
+	WiFi.printDiag(Serial);
 	Serial.print("connecting");
 	while (WiFi.status() != WL_CONNECTED) {
 		Serial.print(".");
@@ -143,6 +185,7 @@ void setup() {
 		Serial.println("MDNS responder started");
 	}
 	server.on("/", handleRoot);
+	server.on("/apidata", handleGetDataApi);
 	server.on("/inline", []() {
 		server.send(200, "text/plain", "this works as well");
 	});
